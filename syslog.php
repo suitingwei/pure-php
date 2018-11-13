1e214c4df7e77be9f5dcf58ea5bb7f45b9e401db
sui <suitingwei@nanzhuxinyu.com>
2018-11-13 11:45:08 +0800

    1.Test the situlation,that fpm will report error when cp the new file into the old file.

---
+++ syslog.php
@@ -0,0 +1,23 @@
+<?php
+$facilities = array(
+    LOG_AUTH,
+    LOG_AUTHPRIV,
+    LOG_CRON,
+    LOG_DAEMON,
+    LOG_KERN,
+    LOG_LOCAL0,
+    LOG_LPR,
+    LOG_MAIL,
+    LOG_NEWS,
+    LOG_SYSLOG,
+    LOG_USER,
+    LOG_UUCP,
+);
+
+for ($i = 0; $i < 10000; $i++) {
+    foreach ($facilities as $facility) {
+        openlog('test', LOG_PID, $facility);
+        syslog(LOG_ERR, "This is a test: " . memory_get_usage(true));
+    }
+}
+?>
