4e92cde35efd7b0eab61d957db90ffe8c092613c
sui <suitingwei@nanzhuxinyu.com>
2019-01-05 16:51:14 +0800

    1. add apcu

--- apcu/index.php
+++ apcu/index.php
@@ -5,3 +5,17 @@
  * Date: 2019/1/5
  * Time: 15:26
  */
+
+$user = $largeArray = $moreLargeArray = null;
+
+$user= apcuFetchOrAdd('user',['name'=>'sui']);
+$largeArray = apcuFetchOrAdd('largeArry',range(1,100000));
+
+return $largeArray;
+function apcuFetchOrAdd($key,$data){
+   if(apcu_exists($key)) {
+       return apcu_fetch($key);
+   }
+   apcu_add($key,$data);
+   return $data;
+}
