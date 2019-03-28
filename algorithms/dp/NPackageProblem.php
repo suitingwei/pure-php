b8f6252ab5e597ababd796a5898d5b90527b8dc9
sui <suitingwei@nanzhuxinyu.com>
2019-03-28 09:16:55 +0800

    Merge master

---
+++ algorithms/dp/NPackageProblem.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sui
+ * Date: 2019-03-27
+ * Time: 22:45
+ */
+
+/**
+ * Class NPackageProblem
+ * 有n 个物品，它们有各自的重量和价值，现有给定容量的背包，如何让背包里装入的物品具有最大的重量
+ */
+class NPackageProblem
+{
+
+}
---
+++ algorithms/string/LongPressedNameProblem.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sui
+ * Date: 2019-03-25
+ * Time: 21:50
+ */
+
+/**
+ * Class LongPressedNameProblem
+ * @link https://leetcode-cn.com/problems/long-pressed-name/
+ */
+class LongPressedNameProblem
+{
+    /**
+     * @param String $name
+     * @param String $typed
+     * @return Boolean
+     */
+    function isLongPressedName($name, $typed) {
+        for($i=0;$i<strlen($name);$i++){
+
+        }
+    }
+}
