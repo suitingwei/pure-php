cbca9db09f7c0bb62be0242954becd5e40149c3a
sui <suitingwei@nanzhuxinyu.com>
2019-04-02 15:44:08 +0800

    1.循环取余。

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <?php
 
-$str = 'suitingwei';
+require __DIR__ . '/vendor/autoload.php';
 
 
-test();
 
-function test(){
-   $str = "qwehnoiqwe" ;
-   
-   echo $str;
+foreach (range(1,100) as $current){
+    list($times,$selectedRound) = calculateRound(1, $current, [30, 20]);
+
+    echo sprintf("当前值:%s，在第%s次完整周期后，在%s区间中\n",$current,$times,$selectedRound);
+}
+
+
+function calculateRound($start, $current, array $rounds)
+{
+    $roundSum = array_sum($rounds);
+    
+    $gap = $current - $start;
+    
+    //计算在第几个周期
+    $times = intval($gap / $roundSum);
+    
+    //对整个大周期取余
+    $current = $current % $roundSum;
+    
+    $selectedRound = 0;
+    
+    foreach ($rounds as $round) {
+        if ($current > $round) {
+            $current -= $round;
+        } else {
+            $selectedRound = $round;
+            break;
+        }
+    }
+    return [$times+1,$selectedRound];
 }
